6e37f554fcc75874559345c4a5a7bfe1cdef05c6
Owain Williams <owain@hedfan.uk>
2026-02-18 16:26:15 +0000

    Add a simple data store

--- ch07/dependency_injection/main.go
+++ ch07/dependency_injection/main.go
@@ -5,3 +5,12 @@ import "fmt"
 func LogOutput(message string) {
 	fmt.Println(message)
 }
+
+type SimpleDataStore struct {
+	userData map[string]string
+}
+
+func (sds SimpleDataStore) UserNameForID(userID string) (string, bool) {
+	name, ok := sds.userData[userID]
+	return name, ok
+}
