07fe93bca99508ba397a8e25c2b0e56c4e77029a
Owain Williams <owain@hedfan.uk>
2026-02-18 16:28:08 +0000

    Define a factory function to create an instance of SimpleDataStore

--- ch07/dependency_injection/main.go
+++ ch07/dependency_injection/main.go
@@ -14,3 +14,12 @@ func (sds SimpleDataStore) UserNameForID(userID string) (string, bool) {
 	name, ok := sds.userData[userID]
 	return name, ok
 }
+
+func NewSimpleDataStore() SimpleDataStore {
+	return SimpleDataStore{
+		userData: map[string]string{
+			"1": "Fred",
+			"2": "Mary",
+			"3": "Pat",
+		}}
+}
