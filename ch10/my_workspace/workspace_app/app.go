139569593a7d81008c5b3e2cd0004e993dc8ab4f
Owain Williams <owain@hedfan.uk>
2026-03-19 14:17:40 +0000

    Create a workspace_app module and import the workspace_lib module

---
+++ ch10/my_workspace/workspace_app/app.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/AnturGyffrous/workspace_lib"
+)
+
+func main() {
+	fmt.Println(workspace_lib.AddNums(2, 3))
+}
---
+++ ch10/my_workspace/workspace_app/go.mod
@@ -0,0 +1,3 @@
+module github.com/AnturGyffrous/workspace_app
+
+go 1.25.0
