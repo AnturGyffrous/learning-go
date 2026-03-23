97fb8e8f8f93df9dbdc45bd2f5ff8167f7b0a058
Owain Williams <owain@hedfan.uk>
2026-03-23 14:24:46 +0000

    Use SubNums from v0.2.0 of workspace_lib

--- ch10/my_workspace/workspace_app/app.go
+++ ch10/my_workspace/workspace_app/app.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/AnturGyffrous/workspace_lib"
 )
 
 func main() {
-	fmt.Println(workspace_lib.AddNums(2, 3))
+	fmt.Println(workspace_lib.SubNums(2, 3))
 }
--- ch10/my_workspace/workspace_app/go.mod
+++ ch10/my_workspace/workspace_app/go.mod
@@ -2,4 +2,4 @@ module github.com/AnturGyffrous/workspace_app
 
 go 1.25.0
 
-require github.com/AnturGyffrous/workspace_lib v0.1.0
+require github.com/AnturGyffrous/workspace_lib v0.2.0
