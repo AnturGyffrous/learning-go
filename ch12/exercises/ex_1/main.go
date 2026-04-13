ea6cf070799dbc60b04b6caaf21190432ef0ba2e
Owain Williams <owain@hedfan.uk>
2026-04-13 17:03:40 +0100

    Refactor to use WaitGroups and a single channel

--- ch12/exercises/ex_1/main.go
+++ ch12/exercises/ex_1/main.go
@@ -1,34 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	quit := make(chan bool)
+	ch := make(chan int)
+	var wg1 sync.WaitGroup
+	wg1.Add(2)
 	go func() {
+		defer wg1.Done()
 		for n := 3; n <= 30; n += 3 {
-			ch1 <- n
+			ch <- n
 		}
-		quit <- true
 	}()
 	go func() {
+		defer wg1.Done()
 		for n := 5; n < 50; n += 5 {
-			ch2 <- n
+			ch <- n
 		}
-		quit <- true
 	}()
-	done := 0
-	for {
-		select {
-		case <-quit:
-			if done++; done > 1 {
-				return
-			}
-		case a := <-ch1:
-			fmt.Println(a)
-		case b := <-ch2:
-			fmt.Println(b)
+	go func() {
+		wg1.Wait()
+		close(ch)
+	}()
+	var wg2 sync.WaitGroup
+	wg2.Add(1)
+	go func() {
+		defer wg2.Done()
+		for n := range ch {
+			fmt.Println(n)
 		}
-	}
+	}()
+	wg2.Wait()
 }
