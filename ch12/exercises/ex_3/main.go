a4f808833b27a1ee59dfb466da7b20920db5616b
Owain Williams <owain@hedfan.uk>
2026-04-14 11:38:39 +0100

    Massively simplify the solution

--- ch12/exercises/ex_3/main.go
+++ ch12/exercises/ex_3/main.go
@@ -8,50 +8,17 @@ import (
 
 func main() {
 	for n := 0; n < 100_000; n += 1000 {
-		s, err := Calculate(n)
-		if err != nil {
-			fmt.Println(n, err)
-		} else {
-			fmt.Println(n, s)
-		}
+		fmt.Println(n, cachedSquareRootMap()[n])
 	}
 }
 
-type SquareRootCalculator interface {
-	Calculate(int) (float64, error)
-}
-
-func Calculate(number int) (float64, error) {
-	squareRoots := cachedSquareRootCalculator()
-	return squareRoots.Calculate(number)
-}
-
-var cachedSquareRootCalculator func() SquareRootCalculator = sync.OnceValue(initSquareRootCalculator)
-
-func initSquareRootCalculator() SquareRootCalculator {
-	fmt.Println("Initializing Square Root Calculator...")
-	m := calculateSquareRoots()
-	return SquareRoots{Map: m}
-}
+var cachedSquareRootMap = sync.OnceValue(calculateSquareRoots)
 
 func calculateSquareRoots() map[int]float64 {
+	fmt.Println("Initializing Square Root Map...")
 	var squareRoots = make(map[int]float64)
 	for i := 0; i < 100_000; i++ {
 		squareRoots[i] = math.Sqrt(float64(i))
 	}
 	return squareRoots
-}
-
-type SquareRoots struct {
-	Map map[int]float64
-}
-
-func (s SquareRoots) Calculate(number int) (float64, error) {
-	if number < 0 {
-		return math.NaN(), fmt.Errorf("number must be positive")
-	}
-	if number >= 100_000 {
-		return math.NaN(), fmt.Errorf("number must be less than 100000")
-	}
-	return s.Map[number], nil
 }
