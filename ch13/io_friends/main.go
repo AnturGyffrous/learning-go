3e1926eece67533651e8332c6b05d51746522e3f
Owain Williams <owain@hedfan.uk>
2026-04-17 12:02:43 +0100

    Create a countLetters func and test it with a Raeder from a string

---
+++ ch13/io_friends/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"strings"
+)
+
+func countLetters(r io.Reader) (map[string]int, error) {
+	buf := make([]byte, 2048)
+	out := map[string]int{}
+	for {
+		n, err := r.Read(buf)
+		for _, b := range buf[:n] {
+			if (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				out[string(b)]++
+			}
+		}
+		if err == io.EOF {
+			return out, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+
+func simpleCountLetters() error {
+	s := "The quick brown fox jumped over the lazy dog"
+	sr := strings.NewReader(s)
+	counts, err := countLetters(sr)
+	if err != nil {
+		return err
+	}
+	fmt.Println(counts)
+	return nil
+}
+
+func main() {
+	err := simpleCountLetters()
+	if err != nil {
+		slog.Error("error with simpleCountLetters", "msg", err)
+	}
+}
