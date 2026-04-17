3f6ab85a378e51601cef78423287c6730bc8081c
Owain Williams <owain@hedfan.uk>
2026-04-17 15:14:28 +0100

    Convert future day from Californian time to London time

--- ch13/time/main.go
+++ ch13/time/main.go
@@ -6,9 +6,21 @@ import (
 )
 
 func main() {
-	t, err := time.Parse("2006-01-02 15:04:05 MST", "1955-11-12 06:04:00 GMT")
+	t, err := time.Parse("2006-01-02 15:04:05 MST", "2015-10-21 16:29:00 PDT")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println(t.Format(time.RFC1123))
+
+	fmt.Println(t.Location(), t.Format(time.RFC1123))
+
+	loc, err := time.LoadLocation("Europe/London")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	t2 := t.In(loc)
+
+	fmt.Println(t2.Location(), t2.Format("Monday, 2 January 2006, 15:04:05 MST (-07:00)"))
 }
